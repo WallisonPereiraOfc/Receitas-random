7479a0a1f07e10deb17ca152a6b77ff83c666062
wallison Pereira <110045968+WallisonPereiraOfc@users.noreply.github.com>
2022-08-15 20:35:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+const resultHeading = document.getElementById('result-heading');
+const mealContainer = document.getElementById('meals');
+const selectedMeal = document.getElementById('selected-meal');
+
+
+function searchMeal(e) {
+    e.preventDefault()
+
+   
+    selectedMeal.innerHTML = '';
+
+   
+    const term = search.value;
+    
+    
+    if(term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
+                if(data.meals === null) {
+                    resultHeading.innerHTML = `<p>There are no search results for '${term}'. Please try a different search.</p>`
+                } else {
+                    mealContainer.innerHTML = data.meals.map( meal => `
+                        <div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            </div>
+                        </div>
+                    `)
+                    .join('')
+                }
+            })
+    } else {
+        alert('Please enter a valid search.')
+    }
+
+ 
+    search.value = '';
+}
+
+
+function getMealById(mealID) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then( res => res.json())
+        .then( data => {
+            const meal = data.meals[0];
+            addMealToDOM(meal);
+        })
+}
+
+
+function addMealToDOM(meal) {
+    const ingredients = [];
+
+    for(let i = 1; i <= 20; i++) {
+        if(meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        } else {
+            break;
+        }
+    };
+
+    selectedMeal.innerHTML = `
+        <div class="selected-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+            <div class="selected-meal-info">
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : '' }
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : '' }
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients</h2>
+                <ul>
+                    ${ingredients.map( ingredient => `<li>${ingredient}</li>` ).join('')}
+                </ul>
+            </div>
+        </div>
+    `;
+}
+
+
+submit.addEventListener('submit', searchMeal);
+
+
+mealContainer.addEventListener('click', e => {
+    const mealInfo = e.path.find( item => {
+        if(item.classList) {
+            return item.classList.contains('meal-info');
+        } else {
+            return false
+        }
+    });
+
+    if(mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealid');
+        getMealById(mealID);
+    }
+
+});
